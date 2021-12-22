6eea755b340f43fb319eef79ae3b6127262fcfef
phastandco <phastandco@gmail.com>
2021-12-22 18:38:06 +0100

    React + Tailwind ok

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1 className="text-3xl font-bold underline w-100 h-100"> One Piece numéro :</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
