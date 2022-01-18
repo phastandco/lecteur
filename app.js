7999ccf7b51a4993ba07d27e44fa3febaaab88fa
Phast <phastandco@gmail.com>
2022-01-18 18:40:11 +0100

    deleted front passage yarn fixing du code bientot ajout de jwt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import Koa from 'koa'
 import {extname, resolve} from 'path'
 import {createReadStream, stat} from 'fs'
 import {promisify} from 'util'
-import jwt from 'koa-jwt'
+//import jwt from 'koa-jwt'
 
 // localhost:3000/api/onepiece?video=D:/1P%20ep/one%20piece%1
 const app = new Koa()
@@ -31,8 +31,6 @@ getToken : ({request}) => request.query.token,
 
 app.use(async ({request, response, state}, next) => {
     //on check que l'url est bien
-    
-
     //infos du token JWT
     console.log("Etat : ", state.user)
     const video = resolve("D:/1P ep", request.query.video)
@@ -70,34 +68,6 @@ app.listen(3000)
 //JWT pour ne pas avoir à savoir si le seveur peut envoyer les vidéos (ça c'est le boulot d'un autre seveur qui se connecte à la bdd)
 //en fait non car bdd au même endroit que le back lol
 
-/**
-var on = document.getElementById("buttonON");
-=======
-import Koa, { HttpError } from 'koa'
->>>>>>> 40aeaef266d814830ee3e69b33f9966cca0d83d7
-
-const app = Koa()
-
-app.listen(3000)
-
-app.use('/api', [...req, res]  = ()=> {
-    HTMLBodyElement('<h1> hello les gens </h1>')
-})
-
-function ON (){
-    var allumage = document.getElementById("buttonON");
-
-    function allumage ()
-    {
-    alert("je suis ON");
-    console.log("OUI OUI");
-    };
-
-    allumage.onclick = allumage;
-}
-
-<<<<<<< HEAD
-on.onclick = allumage;
-*/
+//40aeaef266d814830ee3e69b33f9966cca0d83d7
 
 //url https://ianime-fr.com/voir-one-piece-episode-[NUMBER]-vostfr/
