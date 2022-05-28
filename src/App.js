eed19a15ef2cbfa17d22df1282939ac5e5cd238c
Phast <phastandco@gmail.com>
2022-05-28 14:19:31 +0200

    post pending

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import ListEpisode from "./ListEpisode";
 import Lecteur from "./Lecteur";
 import EpisodeTitle from "./EpisodeTitle"
 import { useEffect, useState} from 'react'
-//
 
 function App() {
 
-  //Pas d'état initial puisqu'on a encore rien récupéré (null) ?
-  const [episode, setEpisode] = useState(null)
-  const [search, setSearch] = useState(1)
+  const [episode, setEpisode] = useState(0)
+  const [searchNumber, setSearch] = useState(1)
 
-  const ipLenovo = '192.168.0.38'
-  const nodePort = ':8080'
+  const ipLenovo = 'localhost'
+  const nodePort = 8080
 
   async function getEpisode(number) {
-    let url = "http://localhost:8080/episode/" + number
+    let url = "http://" + ipLenovo + ":" + nodePort + "/episode/" + number
     console.log("fetch : " + url)
 
     const params = {
@@ -25,29 +23,55 @@ function App() {
 
     let response = await fetch(url, params);
     let episode = await response.json();
-    console.log("episode", episode.fileName)
     setEpisode(episode);
   }
 
-  useEffect(() => {
-    console.log('prends effet');
-  }, [])
+  async function putEpisode(number) {
+    console.log("number", number)
+    let url = "http://" + ipLenovo + ":" + nodePort + "/episode/" + number
+    console.log("fetch : " + url)
 
-  // downloadEpisode();
+    const params = {
+      method: 'POST',
+    };
 
+    let response = await fetch(url, params);
+    if (response)
+      console.log("okok");
+  }
+
+  console.log("episode", episode)
   return (
     <div className="App">
       <header className="App-header">
 
-        <input className="rounded-lg text-gray-600" onChange={e => setSearch(e.target.value)}></input>
+        <input
+        className="rounded-lg text-gray-600"
+        onChange={e => setSearch(e.target.value)}></input>
         <button
-        className="rounded-xl bg-gray-600 hover:bg-gray-700 px-4 py-1 my-2 border-solid"
-        onClick={() => getEpisode(search)}>Download
+          className="rounded-xl bg-gray-600 hover:bg-gray-700 px-4 py-1 my-2 border-solid"
+          onClick={() => getEpisode(searchNumber)}>Download
         </button>
-        {<EpisodeTitle episode={episode}/>}
+        {
+          (episode) ?
+          <EpisodeTitle title={episode.fileName} /> : ''
+        }
+        {
+          ( (episode === null) || (episode.msg === "not found") ) ?
+          <> 
+            <p> Episode Not Found </p>
+            <button
+              className="rounded-xl bg-gray-600 hover:bg-gray-700 px-4 py-1 my-2 border-solid"
+              onClick={() => putEpisode(searchNumber)}>Do you want to add it ?
+            </button>
+            </> :
+            ""
+        }
         </header>
     </div>
   );
 }
+
+
 
 export default App;
--- src/EpisodeTitle.js
+++ src/EpisodeTitle.js
@@ -1,13 +1,5 @@
-import React from "react";
-
-const EpisodeTitle = episode => {
-    if (episode) {
-        console.log("ça marche", episode)
-        return <p> {episode.fileName} </p>
-    } else {
-        console.log("ça marche po", episode)
-        return <p> {episode.fileName} </p>
-    }
+const EpisodeTitle = ({title}) => { 
+    return <h2>{title}</h2>;    
 }
 
 export default EpisodeTitle;
